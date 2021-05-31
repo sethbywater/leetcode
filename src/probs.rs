e73993b8c809f2f694c487a57f45102fd3366639
sethb <seth.bywater@gmail.com>
2021-05-30 21:58:25 -0400

    407. Trapping Rain Water II

--- src/probs.rs
+++ src/probs.rs
@@ -77,8 +77,6 @@ pub fn trap(height: Vec<i32>) -> i32 {
 /// ## Given an m * n integer matrix height_map representing the height of
 /// ## each unit cell in a 2D elevation map, return the volume of water
 /// ## it can trap after raining
-/// Uses a custom Queue type for sorted insertions
-/// TODO Not working
 pub fn trap_rain_water(height_map: Vec<Vec<i32>>) -> i32 {
 	if (height_map.len() < 3) || (height_map[0].len() < 3) { return 0 }
 	let n = height_map.len();
@@ -89,15 +87,22 @@ pub fn trap_rain_water(height_map: Vec<Vec<i32>>) -> i32 {
 	let mut heap: BinaryHeap<(Reverse<i32>, usize, usize)> = BinaryHeap::with_capacity(m * n);
 
 	// Load edges
-	for (i, h) in height_map[0].iter().enumerate() { heap.push((Reverse(*h), 0, i)) }
+	for (i, h) in height_map[0].iter().enumerate() {
+		heap.push((Reverse(*h), 0, i));
+		visited[0][i] = true;
+	}
 	for i in 1..n-1 {
 		heap.push((Reverse(height_map[i][0]), i, 0));
 		heap.push((Reverse(height_map[i][m-1]), i, m-1));
+		visited[i][0] = true;
+		visited[i][m-1] = true;
 	}
-	for (i, h) in height_map[n-1].iter().enumerate() { heap.push((Reverse(*h), n-1, i)) }
+	for (i, h) in height_map[n-1].iter().enumerate() {
+		heap.push((Reverse(*h), n-1, i));
+		visited[n-1][i] = true;
+	}
 
 	while let Some((_, i, j)) = heap.pop() {
-		visited[i][j] = true;
 		let h = height_map[i][j];
 		max = max.max(h);
 		water += max - h;
@@ -105,6 +110,7 @@ pub fn trap_rain_water(height_map: Vec<Vec<i32>>) -> i32 {
 		for (k, l) in neighbors {
 			if k < n && l < m && !visited[k][l] {
 				heap.push((Reverse(height_map[k][l]), k, l));
+				visited[k][l] = true;
 			}
 		}
 	}
