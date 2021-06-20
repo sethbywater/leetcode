82700a4f156e4d30eb3a04823a55f4b23712212c
sethb <seth.bywater@gmail.com>
2021-06-19 22:18:43 -0400

    allow dead code and tests

--- src/p1472_design_browser_history.rs
+++ src/p1472_design_browser_history.rs
@@ -1,43 +1,47 @@
+#[allow(dead_code)]
 struct BrowserHistory {
     mem: Vec<String>,
-	cur: usize,
+    cur: usize,
 }
 
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
+#[allow(dead_code)]
 impl BrowserHistory {
-
+    
     fn new(homepage: String) -> Self {
         BrowserHistory {
-			mem: vec![homepage],
-			cur: 0
-		}
+            mem: vec![homepage],
+            cur: 0
+        }
     }
-    
+
     fn visit(&mut self, url: String) {
-		self.mem = self.mem[..=self.cur].to_vec();
-		self.mem.push(url);
-		self.cur += 1;
+        self.mem = self.mem[..=self.cur].to_vec();
+        self.mem.push(url);
+        self.cur += 1;
     }
-    
+
     fn back(&mut self, steps: i32) -> String {
-		self.cur -= (steps as usize).min(self.cur);
-		self.mem[self.cur].clone()
+        self.cur -= (steps as usize).min(self.cur);
+        self.mem[self.cur].clone()
     }
-    
+
     fn forward(&mut self, steps: i32) -> String {
         self.cur += (steps as usize).min(self.mem.len()-1 - self.cur);
-		self.mem[self.cur].clone()
+        self.mem[self.cur].clone()
     }
 }
 
-// 
-// Your BrowserHistory object will be instantiated and called as such:
-// let obj = BrowserHistory::new(homepage);
-// obj.visit(url);
-// let ret_2: String = obj.back(steps);
-// let ret_3: String = obj.forward(steps);
-// 
+#[test]
+fn example() {
+    let mut bh = BrowserHistory::new("leetcode.com".to_string());
+    assert_eq!(bh.visit("google.com".to_string()), ());
+    assert_eq!(bh.visit("facebook.com".to_string()), ());
+    assert_eq!(bh.visit("youtube.com".to_string()), ());
+    assert_eq!(bh.back(1), "facebook.com".to_string());
+    assert_eq!(bh.back(1), "google.com".to_string());
+    assert_eq!(bh.forward(1), "facebook.com".to_string());
+    assert_eq!(bh.visit("linkedin.com".to_string()), ());
+    assert_eq!(bh.forward(2), "linkedin.com".to_string());
+    assert_eq!(bh.back(2), "google.com".to_string());
+    assert_eq!(bh.back(7), "leetcode.com".to_string());
+}
