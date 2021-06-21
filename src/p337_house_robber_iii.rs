00bad323ed7a8b4894d3512faf0854d37b5606f4
sethb <seth.bywater@gmail.com>
2021-06-21 17:23:14 -0400

    syntax and testing

--- src/p337_house_robber_iii.rs
+++ src/p337_house_robber_iii.rs
@@ -19,21 +19,46 @@ use std::{cell::RefCell, rc::Rc};
 use crate::collections::TreeNode;
 
 pub fn rob_iii(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    if let Some(node) = &root {
-        let (u, nu) = rob_iii_helper(node);
-        return u.max(nu)
+    fn recurse(cur: Rc<RefCell<TreeNode>>) -> (i32, i32) {
+        let mut l = (0, 0);
+        let mut r = (0, 0);
+        if let Some(node) = cur.borrow().left.clone() {
+            l = recurse(node)
+        }
+        if let Some(node) = cur.borrow().right.clone() {
+            r = recurse(node)
+        }
+        (l.1 + r.1, (l.0 + r.0 + cur.borrow().val).max(l.1 + r.1))
     }
-    else { return 0 }
+
+    let (u, nu) = recurse(root.unwrap());
+    u.max(nu)
 }
 
-fn rob_iii_helper(cur: &Rc<RefCell<TreeNode>>) -> (i32, i32) {
-    let mut l = (0, 0);
-    let mut r = (0, 0);
-    if let Some(node) = &cur.borrow().left {
-        l = rob_iii_helper(node)
-    }
-    if let Some(node) = &cur.borrow().right {
-        r = rob_iii_helper(node)
-    }
-    (l.1 + r.1, (l.0 + r.0 + cur.borrow().val).max(l.1 + r.1))
+#[test]
+fn example_1() {
+    let root = TreeNode::root_from_slice(&[
+        Some(3),
+        Some(2),
+        Some(3), None, None, // Leaf
+        None,
+        Some(3),
+        None,
+        Some(1), None, None // Leaf
+    ]);
+    assert_eq!(rob_iii(root), 7)
+}
+
+#[test]
+fn example_2() {
+    let root = TreeNode::root_from_slice(&[
+        Some(3),
+        Some(4),
+        Some(1), None, None, // Leaf
+        Some(3), None, None, // Leaf
+        Some(5),
+        None,
+        Some(1), None, None // Leaf
+    ]);
+    assert_eq!(rob_iii(root), 9)
 }
