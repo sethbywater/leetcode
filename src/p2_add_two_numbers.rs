aa8d734310d14ae54db9307ba65ddcf80c2e5c20
sethb <seth.bywater@gmail.com>
2021-06-20 21:31:45 -0400

    tests

--- src/p2_add_two_numbers.rs
+++ src/p2_add_two_numbers.rs
@@ -3,7 +3,7 @@
 //! one digit and digits stored in reverse order, multiply the two numbers and
 //!return the product as a linked list
 
-use crate::collections::ListNode;
+use crate::collections::{ListNode, list_root_to_vec};
 
 /// Top-level function to call the helper function
 pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
@@ -23,4 +23,39 @@ fn add_three_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>, mut c
 			val: carry % 10
 		}
 	} ))
+}
+
+#[test]
+fn example_1() {
+	// 342 + 465 = 807
+	let l1 = ListNode::root_from_slice(&[2, 4, 3]);
+	let l2 = ListNode::root_from_slice(&[5, 6, 4]);
+	let correct = ListNode::root_from_slice(&[7, 0, 8]);
+	assert_eq!(
+		list_root_to_vec(add_two_numbers(l1, l2)),
+		list_root_to_vec(correct)
+	)
+}
+
+#[test]
+fn example_2() {
+	let l1 = ListNode::root_from_slice(&[0]);
+	let l2 = ListNode::root_from_slice(&[0]);
+	let correct = ListNode::root_from_slice(&[0]);
+	assert_eq!(
+		list_root_to_vec(add_two_numbers(l1, l2)),
+		list_root_to_vec(correct)
+	)
+}
+
+#[test]
+fn example_3() {
+	// 342 + 465 = 807
+	let l1 = ListNode::root_from_slice(&[9, 9, 9, 9, 9, 9, 9]);
+	let l2 = ListNode::root_from_slice(&[9, 9, 9, 9]);
+	let correct = ListNode::root_from_slice(&[8, 9, 9, 9, 0, 0, 0, 1]);
+	assert_eq!(
+		list_root_to_vec(add_two_numbers(l1, l2)),
+		list_root_to_vec(correct)
+	)
 }
