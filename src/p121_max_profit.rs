cd4125a926659ac05c32dadb112f5c6a7dfd04b6
sethb <seth.bywater@gmail.com>
2021-06-22 13:34:42 -0400

    tests

--- src/p121_max_profit.rs
+++ src/p121_max_profit.rs
@@ -1,7 +1,7 @@
 //! # 121. Max profit
-//! ## Given an array indicating the price of a stock on a particular day,
-//! ## return the maximum profit you can acheive by buying on one day and
-//! ## selling on a future day
+//! Given an array indicating the price of a stock on a particular day,
+//! return the maximum profit you can acheive by buying on one day and
+//! selling on a future day
 
 use std::cmp;
 
@@ -14,4 +14,20 @@ pub fn max_profit(v: Vec<i32>) -> i32 {
         min_seen = cmp::min(min_seen, num);
     }
     max_profit
+}
+
+#[test]
+fn example_1() {
+    assert_eq!(
+        max_profit(vec![7, 1, 5, 3, 6, 4]),
+        5
+    )
+}
+
+#[test]
+fn example_2() {
+    assert_eq!(
+        max_profit(vec![7, 6, 4, 3, 1]),
+        0
+    )
 }
